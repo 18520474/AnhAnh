8c6833d725462026aa7cb7642d5f08e7f969c992
18520460 <18520460@gm.uit.edu.vn>
2021-03-15 13:26:15 +0700

    update new python rgb convert code

---
+++ Assigment/weak_2/Python/RGBtoGRAYSCALE.py
@@ -0,0 +1,45 @@
+from cv2 import cv2
+import numpy as np
+import struct
+import fire
+
+def float_to_bin(num):
+    return format(struct.unpack('!I', struct.pack('!f', num))[0], '032b')
+def rgb_to_grayscale(r, g, b):
+    G = (0.2989*r + 0.5870*g + 0.1140*b) / 255
+    return G
+def status(y, i):
+    tmp =  int(y / 10)
+    if(i % tmp == 0):
+        percent = (i / tmp) * 10
+        print("procesing " + str(percent) + '%')
+def printimg(img):
+    img   = cv2.imread(img)
+    b,g,r = cv2.split(img)
+
+    Green = open("Green.txt", "w")
+    Blue  = open("Blue.txt" , "w")
+    Red   = open("Red.txt"  , "w")
+    Gray  = open("Gray.txt" , "w")
+
+    y, x, _ = img.shape          #get x, y resolusion
+    gray_img = np.zeros((y, x))  #create numpy array for storing gray img 
+    for i in range(y):
+        for j in range(x):
+            gray_img[i][j] = rgb_to_grayscale(r[i][j], g[i][j], b[i][j])    #calulate gray img
+            Green.write(np.binary_repr(g[i][j], width=8) + '\n')            #write Green.txt
+            Red.write(np.binary_repr(r[i][j], width=8) + '\n')              #write Red.txt
+            Blue.write(np.binary_repr(b[i][j], width=8) + '\n')             #write Blue.txt
+            Gray.write(float_to_bin(gray_img[i][j]) + '\n')                 #write Gray.txt
+        status(y, i)                                                        #print process status
+
+    if (x > 400) and (y > 400):
+        cv2.imshow('origin', cv2.resize(img,      (400,400), interpolation = cv2.INTER_AREA))
+        cv2.imshow('result', cv2.resize(gray_img, (400,400), interpolation = cv2.INTER_AREA))
+    else:
+        cv2.imshow('origin', img)
+        cv2.imshow('result', gray_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    fire.Fire(printimg)
