5e8aec42d798269c6f7677081b9c0907e717f8e7
tuananh <18520474>
2021-03-20 20:06:38 -0700

    add compare

--- Assigment/week_2/Python/COMPARE_img.py
+++ Assigment/week_2/Python/COMPARE_img.py
@@ -13,25 +13,48 @@ def status(y, i):
 
 Gray     = open("text/Gray.txt", "r")
 Gray_RTL = open("text/Result.txt", "r")
+Compare = open("compare.txt", "w")
 x = 1155
 y = 1024
 python_img  = np.zeros((x , y))
 rtl_img     = np.zeros((x , y))
+count = 0
+sum = 0
+max = 0
+min =  float('inf')
 
 for i in range(x):
     for j in range(y):
         python_line       = Gray.readline()
         rtl_line          = Gray_RTL.readline()
+        test = (abs(bin_to_float(python_line) - bin_to_float(rtl_line)))
+        sum = sum + test
+        if (max < test):
+            max = test
+        if (min > test):
+            min = test
+        Compare.write(str(test) + '\n')
+        count = count + 1
         python_img[i][j]  = bin_to_float(python_line)
         rtl_img[i][j]     = bin_to_float(rtl_line)
     status(x, i)
-
+print("Sai so lon nhat " + str(max) + '\n')
+print("Sai so nho nhat " + str(min) + '\n')
+print("Sai so trung binh " + str(sum/count) + '\n')
 if (x > 400) and (y > 400):
     cv2.imshow('Python', cv2.resize(python_img, (400,400), interpolation = cv2.INTER_AREA))
     cv2.imshow('RTL',    cv2.resize(rtl_img,    (400,400), interpolation = cv2.INTER_AREA))
 else:
     cv2.imshow('Python', python_img)
     cv2.imshow('RTL', rtl_img)
+
+#case = Gray.readline()
+#case2 = Gray_RTL.readline()
+#while case != '':
+    #test = (abs(bin_to_float(case) - bin_to_float(case2)))
+    #Compare.write(str(test) + '\n')
+    #case = Gray.readline()
+    #case2 = Gray_RTL.readline()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
